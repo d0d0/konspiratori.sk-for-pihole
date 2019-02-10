aa5e0975a58aa41bec635a1140b9f7d273435802
jozefduc <jozef.d13@gmail.com>
2019-02-10 13:35:17 +0100

    Add wildcard for subdomains

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const rp = require('request-promise');
-var os = require('os');
+const os = require('os');
 
 const app = express();
 
 app.get('/', function (req, res, next) {
     rp('https://www.konspiratori.sk/assets/downloads/zoznam.txt')
-        .then(function (body) {
-            res.send(body.split(/[\r\n]+/).map(function (obj) {
-                return obj.split(',')[0]
-            }).join(os.EOL))
-        }).catch(next)
-
+        .then(body => res.send(
+            body
+                .split(/[\r\n]+/)
+                .map(obj => obj.split(',')[0])
+                .map(obj => [obj, '*.' + obj].join(os.EOL))
+                .join(os.EOL)
+            )
+        )
+        .catch(next);
 });
 
 app.listen(5000);
