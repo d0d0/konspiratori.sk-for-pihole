737cdb262ce76208e21e403a67e3aea35c840ceb
Jozef Duc <jozef.duc@eset.sk>
2019-10-01 12:22:29 +0200

    Merge pages

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const rp = require('request-promise');
 const os = require('os');
 
 const app = express();
+let blocked = new Set();
 
 app.get('/', function (req, res, next) {
     rp('https://www.konspiratori.sk/assets/downloads/zoznam.txt')
-        .then(body => res.send(
-            body
-                .split(/[\r\n]+/)
-                .map(obj => obj.split(',')[0])
-                .map(obj => [obj, 'www.' + obj].join(os.EOL))
-                .join(os.EOL)
-            )
-        )
-        .catch(next);
+        .then((body) => {
+                const actual = body
+                    .split(/[\r\n]+/)
+                    .map(obj => obj.split(',')[0])
+                    .map(obj => [obj, 'www.' + obj])
+                    .flatMap(obj => obj)
+
+                blocked.add(actual)
+                res.send(Array.from(blocked).join(os.EOL))
+            }
+        ).catch(next);
 });
 
 app.listen(5000);
